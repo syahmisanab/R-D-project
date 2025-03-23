4ef2e6a7f42e8a384b5248667c70b40aa4edfec0
Syaz <115781685+syahmisanab@users.noreply.github.com>
2025-03-23 13:00:19 +0800

    Update connect_4.py

--- connect_4.py
+++ connect_4.py
@@ -1,117 +1,145 @@
-import turtle
+import turtle, random 
 
 screen = turtle.Screen()
 screen.delay(0)
 
-sprite = turtle.Turtle()
-sprite.penup()
-sprite.speed(0)
-sprite.goto(1000, -1000)
-
-chip = sprite.clone()
+chip = turtle.Turtle()
 chip.shape("circle")
 chip.color("green")
+chip.penup()
+chip.speed(0)
+
+CHIP_DIST = 30 # the distance each chip will be apart
 
 connect = []
-CHIP_DIST = 30  # The distance between chips
-ORIGIN_X = -90
-ORIGIN_Y = -100
-RIGHTMOST_COL = ORIGIN_X + CHIP_DIST * 6
 
-for i in range(7):
-    connect.append([])  # Create a column
-    y = connect[i]  # Get the column you just created
-    for j in range(7):  # Add to the column while drawing it
-        chip.goto(ORIGIN_X + i * CHIP_DIST, ORIGIN_Y + j * CHIP_DIST)
+for i in range(7) :
+    connect.append([])
+    x = connect[i]
+    for j in range(7) : 
+        x.append(0)
+        chip.goto(-90 + i * CHIP_DIST, -100 + j * CHIP_DIST)
         chip.stamp()
-        y.append(0)
-
+               
 chip.color("blue")
-chip.goto(ORIGIN_X + 3 * CHIP_DIST, ORIGIN_Y + 7 * CHIP_DIST)
+chip.goto(-90 + 3 * CHIP_DIST, -100 + 7 * CHIP_DIST)
 
+turn = 1
 running = True
 
 def r():
-    if running:
+    if running :
         x = chip.xcor()
-        if x < RIGHTMOST_COL:
+        if x < (-90 + CHIP_DIST * 6) : 
             chip.setx(x + CHIP_DIST)
-        else:
-            chip.setx(ORIGIN_X)
-
-screen.onkey(r, "Right")
-screen.listen()
-
+        else :
+            chip.setx(-90)
+                        
 def l():
-    if running:
+    if running :
         x = chip.xcor()
-        if x > ORIGIN_X:
+        if x > -90 : 
             chip.setx(x - CHIP_DIST)
-        else:
-            chip.setx(RIGHTMOST_COL)
-
-screen.onkey(l, "Left")
-
-turn = 1
-
+        else : 
+            chip.setx(-90 + CHIP_DIST * 6)
+            
 def d():
     global turn, running
-    if running:
-        # Get the column number where the chip dropped
-        x = int((chip.xcor() - ORIGIN_X) / CHIP_DIST)
+    if running :
+        x = int(chip.xcor() / CHIP_DIST) + 3
         col = connect[x]
-
-        for y in range(len(col)):  # Indented correctly
-            if col[y] == 0:
-                chip.sety(ORIGIN_Y + y * CHIP_DIST)
+        for y in range(len(col)) :
+            if(col[y] == 0) :
+                chip.sety( -100 + y*CHIP_DIST)
                 chip.stamp()
-                chip.goto(ORIGIN_X + 3 * CHIP_DIST, ORIGIN_Y + 7 * CHIP_DIST)
+                chip.speed(0)
+                chip.goto(-90 + 3 * CHIP_DIST, -100 + 7 * CHIP_DIST)
                 connect[x][y] = turn
-
-                if haswinner(x, y):  # Check for winner after placing chip
+                if haswinner(x,y) :
                     running = False
-                    style = ('Courier', 15, 'bold')
+                    style= ('Courier', 15, 'bold')
                     turtle.write("Player " + str(turn) + " is the winner!", font=style, align='center')
-                else:
-                    if turn == 1:
+                else :
+                    if turn == 1 : 
                         turn = 2
                         chip.color("red")
-                    else:
+                    else : 
                         turn = 1
                         chip.color("blue")
-                break  # Stop after placing the chip
+                break
+                
+def checkrow(x,y) :
+    count = 1
+    for i in range(1,4) :
+        if x + i < 7 and connect[x + i][y] == turn :
+            count += 1
+        else :
+            break
+    for i in range(1,4) :
+        if x - i > -1 and connect[x - i][y] == turn :
+            count += 1
+        else :
+            break
+    if count >= 4 : 
+        return True
+    return False
 
+def checkcolumn(x,y) :
+    count = 1  
+    for i in range(1,4) :
+        if y + i < 7 and connect[x][y + i] == turn :
+            count += 1
+        else :
+            break
+    for i in range(1,4) :
+        if y - i > -1 and connect[x][y - i] == turn :
+            count += 1
+        else :
+            break
+    if count >= 4 : 
+        return True 
+    return False
+
+def checkdiag1(x,y) :
+    count = 1  
+    for i in range(1,4) :
+        if y + i < 7 and x + i < 7 and connect[x + i][y + i] == turn :
+            count += 1
+        else :
+            break
+    for i in range(1,4) :
+        if y - i > -1 and x - i > -1 and connect[x - i][y - i] == turn :
+            count += 1
+        else :
+            break
+    if count >= 4 : 
+        return True 
+    return False
+
+def checkdiag2(x,y) :
+    count = 1  
+    for i in range(1,4) :
+        if y + i < 7 and x - i > -1 and connect[x - i][y + i] == turn :
+            count += 1
+        else :
+            break
+    for i in range(1,4) :
+        if y - i > -1 and x + i < 7 and connect[x + i][y - i] == turn :
+            count += 1
+        else :
+            break
+    if count >= 4 : 
+        return True 
+    return False
+
+def haswinner(x,y) :        
+    return checkrow(x,y) or checkcolumn(x,y) or checkdiag1(x,y) or checkdiag2(x,y)
+
+
+                        
+screen.onkey(r, "Right")
+screen.onkey(l, "Left")
 screen.onkey(d, "Down")
+screen.listen()
 
-def haswinner(x, y):
-    return checkrow(x, y) or checkcolumn(x, y)
 
-def checkrow(x, y):
-    count = 1
-    for i in range(1, 4):
-        if x + i < 7 and connect[x + i][y] == turn:
-            count += 1
-        else:
-            break
-    for i in range(1, 4):
-        if x - i > -1 and connect[x - i][y] == turn:
-            count += 1
-        else:
-            break
-    return count >= 4
-
-def checkcolumn(x, y):
-    count = 1
-    for i in range(1, 4):
-        if y + i < 7 and connect[x][y + i] == turn:
-            count += 1
-        else:
-            break
-    for i in range(1, 4):
-        if y - i > -1 and connect[x][y - i] == turn:
-            count += 1
-        else:
-            break
-    return count >= 4
-
-screen.mainloop()  # Keeps the window open
